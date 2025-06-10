8da06a1770fce1052296b1106bb7a177ce498c03
Simone Orsi <simahawk@gmail.com>
2025-06-10 08:09:44 +0200

    base_ubl: split in base_ubl_parse + base_ubl_generate

--- base_ubl_generate/__manifest__.py
+++ base_ubl_generate/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Base UBL",
+    "name": "Base UBL generate",
     "version": "17.0.1.0.0",
     "category": "Hidden",
     "license": "AGPL-3",
-    "summary": "Base module for Universal Business Language (UBL)",
+    "summary": "Base module to generate UBL files (Universal Business Language)",
     "author": "Akretion,Onestein,Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/edi",
-    "depends": ["base"],
+    "depends": ["base_ubl"],
     "installable": True,
 }
